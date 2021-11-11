a9c73281fd56f5d2febbe4bdb39a509cc2948731
Mirko Josimovic <mirkoj@software-nation.com>
2021-11-11 15:14:41 +0100

    Implementation of mongoose and connection to DB loader logic

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,7 +1,10 @@
 import expressLoader from './express';
+import mongooseLoader from './mongoose';
 import Logger from './logger';
 
 export default async ({ expressApp }) => {
+  await mongooseLoader();
+  Logger.info('DB loaded and connected!');
   await expressLoader({ app: expressApp });
   Logger.info('Express loaded');
 };
