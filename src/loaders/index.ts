026f555b7b566f193a2d5d5a9553de735c4ad3c9
Mirko Josimovic <mirkoj@software-nation.com>
2021-11-11 17:24:53 +0100

    Dependency injector logic implemented

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,24 @@
 import expressLoader from './express';
 import mongooseLoader from './mongoose';
+import dependencyInjectorLoader from './dependencyInjector';
 import Logger from './logger';
 
 export default async ({ expressApp }) => {
-  await mongooseLoader();
+  const mongoConnection = await mongooseLoader();
   Logger.info('DB loaded and connected!');
+
+  const itemModel = {
+    name: 'itemModel',
+    // Notice the require syntax and the '.default'
+    model: require('../models/item').default,
+  };
+
+  await dependencyInjectorLoader({
+    mongoConnection,
+    models: [itemModel],
+  });
+  Logger.info('Dependency Injector loaded');
+
   await expressLoader({ app: expressApp });
   Logger.info('Express loaded');
 };
